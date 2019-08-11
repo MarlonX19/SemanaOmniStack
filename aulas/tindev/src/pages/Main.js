d180dd6a5d259fc28fece095803db9ef561a8dec
MarlonX19 <marlon.englemam@gmail.com>
2019-08-11 17:09:55 -0300

    Finishing mobile version

--- aulas/tindev/src/pages/Main.js
+++ aulas/tindev/src/pages/Main.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-community/async-storage';
 import { SafeAreaView, View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
@@ -9,7 +9,7 @@ import like from '../assets/like.png';
 import dislike from '../assets/dislike.png';
 
 
-export default function Main({ navigation }){
+export default function Main({ navigation }) {
     const id = navigation.getParam('user');
 
     console.log(id);
@@ -28,23 +28,29 @@ export default function Main({ navigation }){
         loadUsers();
     }, [id]);
 
-    async function handleLike(id) {
-        await api.post(`/devs/${id}/likes`, null, {
+    async function handleLike() {
+        // Recover first user from array of users
+        const [user, ...rest] = users;
+
+        await api.post(`/devs/${user._id}/likes`, null, {
             headers: { user: id }
         });
 
-        setUsers(users.filter(user => user._id != id));
+        setUsers(rest);
     }
 
-    async function handleDislike(id) {
-        await api.post(`/devs/${id}/dislikes`, null, {
+    async function handleDislike() {
+        // Recover first user from array of users
+        const [user, ...rest] = users;
+
+        await api.post(`/devs/${user._id}/dislikes`, null, {
             headers: { user: id },
         });
 
-        setUsers(users.filter(user => user._id != id));
+        setUsers(rest);
     }
 
-    async function handleLogout(){
+    async function handleLogout() {
         await AsyncStorage.clear();
 
         navigation.navigate('Login');
@@ -59,33 +65,35 @@ export default function Main({ navigation }){
             </TouchableOpacity>
 
             <View style={styles.cardContainer}>
-                { users.length === 0 
-                ? <Text style={styles.empty}>Ninguém por perto!</Text> 
-                : 
-                ( users.map((user, index) => (
-                    <View key={user._id} style={[styles.card, { zIndex: users.length - index }]}>
-                    <Image style={styles.avatar} source={{ uri: user.avatar }} />
-                    <View style={styles.footer}>
-                        <Text style={styles.name}>{user.name}</Text>
-                        <Text style={styles.bio} numberOfLines={3}>{user.name}</Text>
-                    </View>
-                </View>
-                ))
-                )
-                 }
+                {users.length === 0
+                    ? <Text style={styles.empty}>Ninguém por perto!</Text>
+                    :
+                    (users.map((user, index) => (
+                        <View key={user._id} style={[styles.card, { zIndex: users.length - index }]}>
+                            <Image style={styles.avatar} source={{ uri: user.avatar }} />
+                            <View style={styles.footer}>
+                                <Text style={styles.name}>{user.name}</Text>
+                                <Text style={styles.bio} numberOfLines={3}>{user.name}</Text>
+                            </View>
+                        </View>
+                    ))
+                    )
+                }
             </View>
 
             <View style={styles.buttonsContainer}>
                 <TouchableOpacity
+                    onPress={handleDislike}
                     style={styles.button}
-                    source={like}
                 >
+                    <Image source={dislike} />
                 </TouchableOpacity>
 
                 <TouchableOpacity
+                    onPress={handleLike}
                     style={styles.button}
-                    source={dislike}
                 >
+                    <Image source={like} />
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
@@ -159,17 +167,18 @@ const styles = StyleSheet.create({
         width: 50,
         height: 50,
         borderRadius: 25,
-         backgroundColor: '#FFF',
-         justifyContent: 'center',
-         marginHorizontal: 20,
-         elevation: 2,
-         shadowColor: '#000',
-         shadowOpacity: 0.05,
-         shadowRadius: 2,
-         shadowOffset: {
-             width: 0,
-             height: 2,
-         }
+        backgroundColor: '#FFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 20,
+        elevation: 2,
+        shadowColor: '#000',
+        shadowOpacity: 0.05,
+        shadowRadius: 2,
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        }
 
     },
 
