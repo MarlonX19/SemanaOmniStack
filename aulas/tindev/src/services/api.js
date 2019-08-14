8e494096edde51baf3897c223cf77f8fa4dbad6d
MarlonX19 <marlon.englemam@gmail.com>
2019-08-14 03:51:05 -0300

    Added Procfile

--- aulas/tindev/src/services/api.js
+++ aulas/tindev/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    //Using laptop's ip because of android emulator
-    baseURL: 'http://192.168.15.18:3333'
+    //Using laptop's ip because of android emulator: http://192.168.15.18:3333
+    baseURL: 'https://tindevtest.herokuapp.com/'
+    //Heroku URL: https://tindevtest.herokuapp.com/
 });
 
 export default api;
